98f5dbbb701c888ba4a258e953082c657bc7b93e
CrisCanonDev <criscanon21@gmail.com>
2024-03-06 16:46:11 +0200

    Adding lombok annotations to model

--- form-management-thymeleaf/src/main/java/com/ccdev/formsthymeleaf/model/User.java
+++ form-management-thymeleaf/src/main/java/com/ccdev/formsthymeleaf/model/User.java
@@ -1,7 +1,16 @@
 package com.ccdev.formsthymeleaf.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private String email;
