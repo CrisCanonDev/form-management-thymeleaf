e7ff4113b526a4c0b13d0096c1ba44f84e124f62
CrisCanonDev <criscanon21@gmail.com>
2024-03-06 16:42:35 +0200

    First commit

---
+++ form-management-thymeleaf/src/main/java/com/ccdev/formsthymeleaf/FormManagementThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.ccdev.formsthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormManagementThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormManagementThymeleafApplication.class, args);
+	}
+
+}
---
+++ form-management-thymeleaf/src/test/java/com/ccdev/formsthymeleaf/FormManagementThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccdev.formsthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormManagementThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
