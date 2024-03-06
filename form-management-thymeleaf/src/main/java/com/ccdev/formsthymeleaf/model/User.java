7fa6f1da1f7ddf3796b1872e6968da5d5d1b0ec9
CrisCanonDev <criscanon21@gmail.com>
2024-03-06 16:45:33 +0200

    Adding user model data field

---
+++ form-management-thymeleaf/src/main/java/com/ccdev/formsthymeleaf/model/User.java
@@ -0,0 +1,13 @@
+package com.ccdev.formsthymeleaf.model;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String gender;
+    private Date dateOfBirth;
+    private String additionalInformation;
+
+}
